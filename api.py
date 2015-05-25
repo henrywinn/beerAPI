19eb8a345859bee86e4330ef2637bc02dd8a1390
Bobby Unverzagt <robert.unverzagt@gmail.com>
2015-05-25 08:17:07 -0600

    possible check for nonunique api keys

--- api.py
+++ api.py
@@ -60,6 +60,8 @@ class Keychain:
     def create_user_key(username, expire_in=timedelta(days=7)):
         expiration = datetime.now() + expire_in
         key = str(uuid.uuid4())
+        while db.search('APIkeys', 'key="'+key+'"') is not []:      # test that key is unique,
+            key = str(uuid.uuid4())                                 # replace if it is
         response = db.put('APIkeys',key,{
             "key": key,
             "username": username,
